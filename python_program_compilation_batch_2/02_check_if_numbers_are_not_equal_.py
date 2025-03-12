7bacd584ab0c7123e587e36ff02481965b513121
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:22:22 +0800

    program_03

--- python_program_compilation_batch_2/02_check_if_numbers_are_not_equal_.py
+++ python_program_compilation_batch_2/02_check_if_numbers_are_not_equal_.py
@@ -1,8 +1,8 @@
 # ask user for input
-input_first_number = int(input("Enter your first number: "))
-input_second_number = int(input("Enter your second number: "))
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
 # check if numbers are equal or not equal
-if input_first_number != input_second_number:
+if first_number != second_number:
     # print not equal if numbers are not equal
     print("NOT EQUAL")
 else:
--- python_program_compilation_batch_2/03_difference_of_two_numbers.py
+++ python_program_compilation_batch_2/03_difference_of_two_numbers.py
@@ -1,3 +1,7 @@
 # ask user for input
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
 # substract the numbers
+result = first_number - second_number
 # print the result
+print(f"The difference is: {result}")
