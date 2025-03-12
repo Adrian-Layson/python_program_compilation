ff18db49eb0b037232ce91bab0809fc750d434e6
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:40:52 +0800

    pseudocode_for_prog07

---
+++ python_program_compilation_batch_2/07_even_number_counter.py
@@ -0,0 +1,6 @@
+# make a variable with 0 value where we will be adding the even numbers
+# make a function with a range of 10 
+# input 10 numbers
+# check if numbers are even
+# add a count to our variable for every even number
+# print the result
