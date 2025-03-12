359fe28b4b5ea9f3be0106ddb011b125e6092cdc
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:52:53 +0800

    pseudocode_for_prog09

---
+++ python_program_compilation_batch_2/09_print_all_numbers_from_0_to_100_except_10_or_5.py
@@ -0,0 +1,5 @@
+# make a variable with a value of 0 which where we will start
+# initialize a while loop that runs until the number is greater than 100
+# check if the number is not divisible by 10 and 5
+# print the current value of number if it doesn't meet the condition
+# increment num by 1 to check next number
