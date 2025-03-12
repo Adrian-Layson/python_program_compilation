65f410886a274954ba7548d952b8d76bac5fe031
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:44:08 +0800

    program_07

--- python_program_compilation_batch_2/07_even_number_counter.py
+++ python_program_compilation_batch_2/07_even_number_counter.py
@@ -1,6 +1,12 @@
 # make a variable with 0 value where we will be adding the even numbers
+even_count = 0
 # make a function with a range of 10 
-# input 10 numbers
+for i in range(10):
+   # input 10 numbers
+    numbers = int(input(f"Enter the {i + 1} number: "))
 # check if numbers are even
-# add a count to our variable for every even number
+    if numbers % 2 == 0:
+        # add a count to our variable for every even number
+        even_count += 1
 # print the result
+print(f"There are {even_count} even number: ")
