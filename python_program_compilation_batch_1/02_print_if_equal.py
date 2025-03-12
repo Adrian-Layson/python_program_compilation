3c732cd2bd2bd7bb57d3ed79cd26ec203652ba03
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 14:14:31 +0800

    program_02

--- python_program_compilation_batch_1/02_print_if_equal.py
+++ python_program_compilation_batch_1/02_print_if_equal.py
@@ -3,7 +3,8 @@ first_number = int(input("Input the first number: "))
 second_number = int(input("Input the second number: "))
 # compare  the two input
 if first_number == second_number: 
+   # check if equal and print
     print("Equal") 
 else:
     print("Not Equal")
-    # check if equal and print
+    
--- python_program_compilation_batch_1/03_sum_of_two_numbers.py
+++ python_program_compilation_batch_1/03_sum_of_two_numbers.py
@@ -1,3 +1,4 @@
 # ask for input of two numbers
 # add the two numbers
 # print the sum
+
