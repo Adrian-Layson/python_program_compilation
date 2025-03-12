4d5f3352db0282efc92bee6ed4b6a5ed65ca3769
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:34:31 +0800

    pseudocode_for_prog06

---
+++ python_program_compilation_batch_2/06_first_number_minus_remaining_numbers.py
@@ -0,0 +1,6 @@
+# input the first number
+# give the value of the first number to a variable
+# make a function with a range of 9 since we already have the first number
+# input the remaining numbers
+# substract to the first number
+# print the result
