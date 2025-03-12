c43b3a3ee5d1c07d385c5b4fcf5e86e07553a6d0
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 14:38:22 +0800

    program_05

--- python_program_compilation_batch_1/05_quotient_of_two_numbers.py
+++ python_program_compilation_batch_1/05_quotient_of_two_numbers.py
@@ -1,3 +1,7 @@
 # ask user for two input
+first_number = float(input("Input the first number: "))
+second_number = float(input("Input the second number: "))
 # divide the two numbers
+result = first_number / second_number
 # print the quotient
+print(f"The quotient is: {result}")
