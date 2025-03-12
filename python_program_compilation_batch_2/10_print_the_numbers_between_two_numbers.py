36576e66619fcce5774a0a77e13f097380e6ea56
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:59:37 +0800

    pseudocode_for_prog10

---
+++ python_program_compilation_batch_2/10_print_the_numbers_between_two_numbers.py
@@ -0,0 +1,4 @@
+# ask user for two numbers
+# check if the first number is greater than the second number, swap if first number is greater than second number
+# for loop all the numbers between the first and second number, excluding both
+# print the result
