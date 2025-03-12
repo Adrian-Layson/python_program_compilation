34d1dff15859ce060e4a055f604ffaec370fd0b3
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 14:10:48 +0800

    Pseudocode for prog3

--- python_program_compilation_batch_1/02_print_if_equal.py
+++ python_program_compilation_batch_1/02_print_if_equal.py
@@ -1,7 +1,9 @@
+# ask user for two input
 first_number = int(input("Input the first number: "))
 second_number = int(input("Input the second number: "))
-
-if first_number == second_number:
-    print("Equal")
+# compare  the two input
+if first_number == second_number: 
+    print("Equal") 
 else:
     print("Not Equal")
+    # check if equal and print
---
+++ python_program_compilation_batch_1/03_sum_of_two_numbers.py
@@ -0,0 +1,3 @@
+# ask for input of two numbers
+# add the two numbers
+# print the sum
