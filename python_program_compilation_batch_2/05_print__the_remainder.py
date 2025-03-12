eebf4a3857fa07ba56985a39b3b12f8cfce3a1a9
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:31:30 +0800

    program_05

--- python_program_compilation_batch_2/05_print__the_remainder.py
+++ python_program_compilation_batch_2/05_print__the_remainder.py
@@ -1,3 +1,7 @@
 # ask user for input
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
 # use the  % function to only get the remainder
+result = first_number % second_number
 # print the remainder
+print(f"The remainder is: {result}")
