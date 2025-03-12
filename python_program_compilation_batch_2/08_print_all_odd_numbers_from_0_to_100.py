2777634bb92d82bfb867b6febf462878fd3f17be
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:47:16 +0800

    pseudocode_for_prog08

---
+++ python_program_compilation_batch_2/08_print_all_odd_numbers_from_0_to_100.py
@@ -0,0 +1,4 @@
+# give a variable a value of 1 since it's the first odd number
+# start a while loop that continues until num is greater than 100
+# print the current value of num (an odd number)
+# Increment num by 2 to get the next odd number
