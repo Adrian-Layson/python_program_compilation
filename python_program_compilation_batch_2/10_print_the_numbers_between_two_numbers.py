dc1f42d7ca2697da82f6de29b139dd109d417b92
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 19:03:22 +0800

    checking_if_first_number_is_greater_than_second number

--- python_program_compilation_batch_2/10_print_the_numbers_between_two_numbers.py
+++ python_program_compilation_batch_2/10_print_the_numbers_between_two_numbers.py
@@ -1,4 +1,9 @@
 # ask user for two numbers
+first_number = int(input("Enter your first number: "))
+second_number = int(input("Enter your second number: "))
 # check if the first number is greater than the second number, swap if first number is greater than second number
+if first_number > second_number:
+    first_number, second_number = second_number, first_number
 # for loop all the numbers between the first and second number, excluding both
-# print the result
+ # print the result
+    
