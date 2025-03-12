b7a2f905da90c19dded37464451fc18339126d21
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-12 18:09:28 +0800

    program_01

--- python_program_compilation_batch_2/01_rename_the_smaller_number.py
+++ python_program_compilation_batch_2/01_rename_the_smaller_number.py
@@ -1,3 +1,9 @@
 # ask user for two inputs
+input_first_number = int(input("Enter your first number: "))
+input_second_number = int(input("Enter your second number: "))
 # compare the two inputs
-# print the smaller number
+if input_first_number > input_second_number:
+   # print the smaller number
+    print(f"The smaller number is: {input_second_number}")
+else:
+    print(f"The smaller number is: {input_first_number}")
