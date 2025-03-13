c00260016c212dd0913275f211891c8f1699d770
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 15:38:39 +0800

    pseudocode_for_prog01

---
+++ python_program_compilation_batch_3/01_display_numbers_that_don't_have_duplicate.py
@@ -0,0 +1,5 @@
+#  make a list to store the numbers that will be inputted
+# ask user for 10 inputs
+# display the numbers that don't have duplicates
+# check each number in the list
+# if the count of number is 1, it's unique
