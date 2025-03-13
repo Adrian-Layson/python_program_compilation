d54f67273ddc56e696fb606df7378102360ee93a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 16:38:32 +0800

    program_04

--- python_program_compilation_batch_3/04_lowest_number_finder.py
+++ python_program_compilation_batch_3/04_lowest_number_finder.py
@@ -9,4 +9,9 @@ while True:
     try:
         number = float(user_input) # checks if input is a valid number
         numbers.append(number) # adds valid number to the list
-    
+    except ValueError:
+        if len(numbers) > 0:
+            print(f"The lowest number inputted is: {min(numbers)}") # prints the lowest number inputted
+        else:
+            print("No valid numbers were entered.")
+        break
