4e487538e98066199d2101a0bba61c71ef3e556e
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 16:06:55 +0800

    pseudocode_for_prog04

---
+++ python_program_compilation_batch_3/04_lowest_number_finder.py
@@ -0,0 +1,5 @@
+# create a list to store inputs
+# ask user for input
+# check if the input is a valid number
+# store the number as an integer
+# if input is not valid, exit the loop and show the lowest number
