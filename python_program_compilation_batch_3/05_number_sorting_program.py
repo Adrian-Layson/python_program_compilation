ff397153aa55da829b5e63b024affa2f06d64d8f
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 16:42:57 +0800

    pseudocode_for_prog05

---
+++ python_program_compilation_batch_3/05_number_sorting_program.py
@@ -0,0 +1,7 @@
+# create a list to store inputted numbers
+# ask user to input a number
+# convert the input to a float and add to the list
+# check if input is valid or invalid
+# sort the list in ascending order
+# print all the numbers added to in the list
+# exit the loop when invalid input
