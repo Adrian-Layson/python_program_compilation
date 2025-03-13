6298b9de797366901e1828cb0a429b998685a8ca
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 16:14:10 +0800

    input_function_working

--- python_program_compilation_batch_3/04_lowest_number_finder.py
+++ python_program_compilation_batch_3/04_lowest_number_finder.py
@@ -1,5 +1,12 @@
 # create a list to store inputs
-# ask user for input
+numbers = []
+
+while True:
+    # ask user for input
+    user_input = input("Enter a number: ")
+
 # check if the input is a valid number
-# store the number as an integer
+    if user_input.isdigit() or (user_input[0] == '-' and user_input[1:].isdigit()):
+        numbers.append(int(user_input)) # store the number as an integer
+
 # if input is not valid, exit the loop and show the lowest number
