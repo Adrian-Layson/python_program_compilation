1824adcb9367639081e4fefaecde05daf994a5d9
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 15:55:08 +0800

    pseudocode_for_prog03

---
+++ python_program_compilation_batch_3/03_unique_duplicate_checker.py
@@ -0,0 +1,5 @@
+# create an empty list to store user inputs
+# ask user to input a number
+# check if the numbe has a duplicate
+# add the number to the list for future comparison
+# if user enters a non-numerical input, exit the loop
