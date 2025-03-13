45d87503a38dd2a3b66f3bfb0a02821e1b4341c0
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 16:37:52 +0800

    tried_a_new_method_to_check_if_input_is_valid_or_not

--- python_program_compilation_batch_3/04_lowest_number_finder.py
+++ python_program_compilation_batch_3/04_lowest_number_finder.py
@@ -2,16 +2,11 @@
 numbers = []
 
 while True:
-    # ask user for input
+    #ask user for input
     user_input = input("Enter a number: ")
 
-    # check if the input is a valid number
-    if user_input.replace('-', '', 1).isdigit() or (user_input[0] == '-' and user_input[1:].replace('.', '', 1).isdigit()):
-        numbers.append(int(user_input)) # store the number as an integer
-    else:
-    # if input is not valid, exit the loop and show the lowest number
-        if len(numbers) > 0:
-            print(f"The lowest number inputted is: {min(numbers)} ")
-        else:
-            print("No valid numbers were entered.")
-        break
+    #checks if input is a valid number
+    try:
+        number = float(user_input) # checks if input is a valid number
+        numbers.append(number) # adds valid number to the list
+    
