59802bc78358c657786b029a1d3337f3f2c68475
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-13 15:47:46 +0800

    pseudocode_for_prog02

---
+++ python_program_compilation_batch_3/02_display_numbers_with_duplicate_display_only_the_first_entry.py
@@ -0,0 +1,4 @@
+# make a list where we will store the inputs
+# ask user for 10 inputs
+# display all the numbers, but only show the first entry of duplicates
+# use seen to track the numbersss that are already displayed
