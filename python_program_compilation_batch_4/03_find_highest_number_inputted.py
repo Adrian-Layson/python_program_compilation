535df1457e5d48872cf4e1d2260ed72d6c862817
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:01:00 +0800

    program 03 final

--- python_program_compilation_batch_4/03_find_highest_number_inputted.py
+++ python_program_compilation_batch_4/03_find_highest_number_inputted.py
@@ -1,5 +1,15 @@
 # create a list to store values
+numbers = []
+
 # ask user for input until invalid
-# checks if input is a digit
-# checks if input is a digit
+while True:
+    number = input("Please input a number: ")
+    if number.isdigit(): # checks if input is a digit
+        numbers.append(int(number))
+    else:
+        print("Invalid input")
+        break
+
 # check and print the highest number submitted
+if numbers:
+    print(f"The highest number inputted is: {max(numbers)}")
