f9b4691135b3ed58651e07e6dae2424e473e898e
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:32:26 +0800

    program 08 final

--- python_program_compilation_batch_2/08_print_all_odd_numbers_from_0_to_100.py
+++ python_program_compilation_batch_2/08_print_all_odd_numbers_from_0_to_100.py
@@ -1,8 +1,7 @@
 # give a variable a value of 1 since it's the first odd number
 number = 1
+
 # start a while loop that continues until num is greater than 100
 while number <= 100:
-   # print the current value of num (an odd number)
-   print(number)
-   # Increment num by 2 to get the next odd number
-   number += 2
+   print(number) # print the current value of num (an odd number)
+   number += 2 # Increment num by 2 to get the next odd number
