a6c005f5e3fe3d7cd0d65b36954c97bdb3a6644c
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:00:13 +0800

    program 04 final final

--- python_program_compilation_batch_1/04_product_of_two_numbers.py
+++ python_program_compilation_batch_1/04_product_of_two_numbers.py
@@ -1,9 +1,9 @@
-# input two numbers from user
+# Input two numbers from user
 first_number = int(input("Input the first number: "))
 second_number = int(input("Input the second number: "))
 
-# multiply the two numbers
+# Multiply the two numbers
 result = first_number * second_number
 
-# print the product
+# Print the product
 print(f"The product is: {result}")
