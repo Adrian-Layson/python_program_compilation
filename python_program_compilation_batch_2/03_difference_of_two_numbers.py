80b6d0b57fabd4be01f37fe8672b652ac316332c
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:08:27 +0800

    program 03 final final

--- python_program_compilation_batch_2/03_difference_of_two_numbers.py
+++ python_program_compilation_batch_2/03_difference_of_two_numbers.py
@@ -1,9 +1,9 @@
-# ask user for input
+# Ask user for input
 first_number = int(input("Enter your first number: "))
 second_number = int(input("Enter your second number: "))
 
-# substract the numbers
+# Substract the numbers
 result = first_number - second_number
 
-# print the result
+# Print the result
 print(f"The difference is: {result}")
