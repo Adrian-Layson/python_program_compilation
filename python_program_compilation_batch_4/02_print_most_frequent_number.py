8339711d464950eb4610eafc2609f6b7a67b57e8
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 13:22:03 +0800

    checks if input is a number

--- python_program_compilation_batch_4/02_print_most_frequent_number.py
+++ python_program_compilation_batch_4/02_print_most_frequent_number.py
@@ -1,4 +1,10 @@
 # create a list to store values
+numbers = []
+
 # ask user for input until invalid
-# checks if input is a digit
+while True:
+    number = input("Please enter a number: ")
+    if number.isdigit(): # checks if input is a digit
+
+
 # check and print the highest number submitted
