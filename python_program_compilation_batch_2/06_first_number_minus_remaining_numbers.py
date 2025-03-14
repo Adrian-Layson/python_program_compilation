d3e474182ec40f2caa8cc28d57670330b330961d
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:10:00 +0800

    program 06 final final

--- python_program_compilation_batch_2/06_first_number_minus_remaining_numbers.py
+++ python_program_compilation_batch_2/06_first_number_minus_remaining_numbers.py
@@ -1,13 +1,13 @@
-# input the first number
+# Input the first number
 first_number = int(input("Enter the first number: "))
 
-# give the value of the first number to a variable
+# Give the value of the first number to a variable
 result = first_number
 
-# make a function with a range of 9 since we already have the first number
+# Make a function with a range of 9 since we already have the first number
 for i in range(9):
-    number = int(input(f"Enter your {i + 2} number: ")) # input the remaining numbers
-    result -= number # substract to the first number
+    number = int(input(f"Enter your {i + 2} number: ")) # Input the remaining numbers
+    result -= number # Substract to the first number
 
-# print the result
+# Print the result
 print(result)
