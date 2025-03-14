4b5f19732879ae747338510049ccf4e0ed14472a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:08:48 +0800

    program 04 final final

--- python_program_compilation_batch_2/04_quotient_of_numbers_without_decimal.py
+++ python_program_compilation_batch_2/04_quotient_of_numbers_without_decimal.py
@@ -1,9 +1,9 @@
-# ask user for input (use int to not include decimals)
+# Ask user for input (use int to not include decimals)
 first_number = int(input("Enter your first number: "))
 second_number = int(input("Enter your second number: "))
 
-# divide the inputted number
+# Divide the inputted number
 result = first_number // second_number
 
-# print the result
+# Print the result
 print(f"The quotient is {result}")
