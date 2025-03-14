a8708efab31933927bfc5527d53fdbfaad58e7e3
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 13:16:01 +0800

    invalid input checker working

--- python_program_compilation_batch_4/02_print_most_frequent_number.py
+++ python_program_compilation_batch_4/02_print_most_frequent_number.py
@@ -1,3 +1,13 @@
 # create a list to store inputs
+numbers = []
+
 # ask user for input until invalid
+while True:
+    try:
+        number = int(input("Please input a number: "))
+        numbers.append(number)
+    except ValueError:
+        print("Invalid input, stopping.")
+        break
+    
 # find the most frequent number
