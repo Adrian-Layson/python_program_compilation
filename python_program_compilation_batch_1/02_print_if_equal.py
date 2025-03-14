a46d8ac737fa6459dd825969d66660b6d0e73469
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:59:28 +0800

    program 02 final final

--- python_program_compilation_batch_1/02_print_if_equal.py
+++ python_program_compilation_batch_1/02_print_if_equal.py
@@ -1,11 +1,11 @@
-# ask user for two input
+# Ask user for two input
 first_number = int(input("Input the first number: "))
 second_number = int(input("Input the second number: "))
 
-# compare  the two input
+# Compare  the two input
 if first_number == second_number: 
    
-   # check if equal and print
+   # Check if equal and print
     print("Equal") 
 else:
     print("Not Equal")
