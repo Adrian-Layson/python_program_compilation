b15d18ddf449a536810d3c56b0471cbef5419ff1
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:59:02 +0800

    program 01 final final

--- python_program_compilation_batch_1/01_find_larger_number.py
+++ python_program_compilation_batch_1/01_find_larger_number.py
@@ -1,8 +1,8 @@
-# ask two inputs from user
+# Ask two inputs from user
 input_first_number = int(input("Enter your first number: "))
 input_second_number = int(input("Enter your second number: "))
 
-# compare the two inputs
+# Compare the two inputs
 if input_first_number > input_second_number:
     print(f"The larger number is: {input_first_number} ") # find the larger number and print
 elif input_first_number < input_second_number:
