95df171f411efd2eed58c382a6510a769f08435a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:38:21 +0800

    program 02 final

--- python_program_compilation_batch_3/02_display_numbers_with_duplicate_display_only_the_first_entry.py
+++ python_program_compilation_batch_3/02_display_numbers_with_duplicate_display_only_the_first_entry.py
@@ -1,9 +1,11 @@
 # make a list where we will store the inputs
 numbers = []
+
 # ask user for 10 inputs
 for i in range(10):
     num = float(input(f"Enter your {i + 1} number: "))
     numbers.append(num)
+
 # display all the numbers, but only show the first entry of duplicates
 print("Numbers: ")
 seen = set() # use seen to track the numbersss that are already displayed
