ac27f2ed448e916742f898dc5b693aabab049399
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:22:41 +0800

    program 07 final

--- python_program_compilation_batch_1/07_sum_of_ten_numbers.py
+++ python_program_compilation_batch_1/07_sum_of_ten_numbers.py
@@ -1,9 +1,12 @@
 # set the total to 0
 total = 0
+
 # ask for 10 inputs
 for i in range(10):
     number = int(input(f"Enter number {i + 1}: "))
+
 # add all the numbers inputted 
     total += number
+
 # print the sum
 print(f"The sum of all numbers is : {total}")
