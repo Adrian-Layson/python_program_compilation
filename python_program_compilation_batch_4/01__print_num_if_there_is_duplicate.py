1f070f6ee8e8958b41fb7438628ef7e6aedfabd4
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:51:09 +0800

    program 01 final

--- python_program_compilation_batch_4/01__print_num_if_there_is_duplicate.py
+++ python_program_compilation_batch_4/01__print_num_if_there_is_duplicate.py
@@ -1,15 +1,15 @@
-# create an empty list
+# Create an empty list
 numbers = []
 
-# ask user for input
+# Ask user for input
 for i in range(10):  
     input_number = int(input(f"Enter your {i + 1} number: "))
     numbers.append(input_number) # add the inputs to the list numbers
 
-# print the numbers that have duplicates
+# Print the numbers that have duplicates
 print("Numbers that have duplicates: ")
 
-# loop through each number in the list, and if the count of the number is greater than 1, print the number.
+# Loop through each number in the list, and if the count of the number is greater than 1, print the number.
 for current_number in set(numbers):  
     if numbers.count(current_number) > 1:  
         print(current_number)
