02df963dfc48970a2c47092d3d1c53dfeaf4beeb
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:58:10 +0800

    program 05 final final

--- python_program_compilation_batch_3/05_number_sorting_program.py
+++ python_program_compilation_batch_3/05_number_sorting_program.py
@@ -1,23 +1,22 @@
-# create a list to store inputted numbers
+# Create a list to store inputted numbers
 numbers = []
 
 while True:
-    # ask user to input a number
+    # Ask user to input a number
     user_input = input("Enter a number: ")
 
     try:
-        # convert the input to a float and add to the list
+        # Convert the input to a float and add to the list
         user_number = float(user_input)
         numbers.append(user_number)
     except ValueError:
         
-        # check if the input is valid or invalid
+        # Check if the input is valid or invalid
         if len(numbers) > 0:
-            
-            # sort the list in ascending order
+            # Sort the list in ascending order
             numbers.sort()
             
-            # print all the numbers added to in the list
+            # Print all the numbers added to in the list
             print("Numbers from lowest to highest are:")
             for num in numbers:
                 print(num)
