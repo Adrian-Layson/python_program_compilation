6d065a005d7d792d7f336b5d21793e12339ff714
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:07:43 +0800

    program 02 final final

--- python_program_compilation_batch_2/02_check_if_numbers_are_not_equal_.py
+++ python_program_compilation_batch_2/02_check_if_numbers_are_not_equal_.py
@@ -1,11 +1,11 @@
-# ask user for input
+# Ask user for input
 first_number = int(input("Enter your first number: "))
 second_number = int(input("Enter your second number: "))
 
-# check if numbers are equal or not equal
+# Check if numbers are equal or not equal
 if first_number != second_number:
     
-    # print NOT EQUAL if numbers are not 
+    # Print NOT EQUAL if numbers are not 
     print("NOT EQUAL")
 else:
     print("EQUAL")
