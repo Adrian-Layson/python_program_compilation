9adc5bc29ee87102bfba7aec76abeabf6ca31889
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:07:14 +0800

    input checker working properly

--- python_program_compilation_batch_4/04_sort_numbers_highest_to_lowest.py
+++ python_program_compilation_batch_4/04_sort_numbers_highest_to_lowest.py
@@ -1,3 +1,13 @@
 # create a list to store inputs
+numbers = []
+
 # ask user for input until invalid
+while True:
+    number = input("Please input a number: ")
+    if number.isdigit():
+        numbers.append(int(number))
+    else:
+        print("Invalid input")
+        break
+
 # sort numbers from highest to lowest and print
