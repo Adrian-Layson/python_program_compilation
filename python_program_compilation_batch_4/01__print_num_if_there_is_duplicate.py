464d0d97306a87b754c8eb45a964ed665f1a99e1
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 13:04:09 +0800

    pseudocode for prog01

---
+++ python_program_compilation_batch_4/01__print_num_if_there_is_duplicate.py
@@ -0,0 +1,5 @@
+# create an empty list
+# ask user for input
+# add the inputs to the list numbers
+# print "numbers that have duplicates: "
+# loop through each number in the list, and if the count of the number is greater than 1, print the number.
