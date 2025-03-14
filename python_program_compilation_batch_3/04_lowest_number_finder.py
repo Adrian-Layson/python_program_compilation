6e2a3127603a8b191a95aa41329797341e88ea29
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:57:20 +0800

    program 04 final final

--- python_program_compilation_batch_3/04_lowest_number_finder.py
+++ python_program_compilation_batch_3/04_lowest_number_finder.py
@@ -1,17 +1,17 @@
-# create a list to store inputs
+# Create a list to store inputs
 numbers = []
 
 while True:
-    #ask user for input
+    #Ask user for input
     user_input = input("Enter a number: ")
 
-    #checks if input is a valid number
+    #Checks if input is a valid number
     try:
-        user_number = float(user_input) # checks if input is a valid number
-        numbers.append(user_number) # adds valid number to the list
+        user_number = float(user_input) # Checks if input is a valid number
+        numbers.append(user_number) # Adds valid number to the list
     except ValueError:
         if len(numbers) > 0:
-            print(f"The lowest number inputted is: {min(numbers)}") # prints the lowest number inputted
+            print(f"The lowest number inputted is: {min(numbers)}") # Prints the lowest number inputted
         else:
             print("No valid numbers were entered.")
         break
