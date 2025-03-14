05b15060dd032322feb9691e5c84cee0733f42cc
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:25:51 +0800

    program 10 final

--- python_program_compilation_batch_1/10_exclude_numbers_ending_with_zero.py
+++ python_program_compilation_batch_1/10_exclude_numbers_ending_with_zero.py
@@ -1,6 +1,4 @@
 # set the range to 101
 for result in range(101):
-    # check if number has 0
-    if result % 10 != 0:
-        #print the result
-        print(result)
+    if result % 10 != 0: # check if number has 0
+        print(result) #print the result
