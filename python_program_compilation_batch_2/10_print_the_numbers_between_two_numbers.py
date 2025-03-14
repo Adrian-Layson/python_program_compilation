7d55f12b24ba809bd1f4620b80fe4e553370780c
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:12:08 +0800

    program 10 final final

--- python_program_compilation_batch_2/10_print_the_numbers_between_two_numbers.py
+++ python_program_compilation_batch_2/10_print_the_numbers_between_two_numbers.py
@@ -1,13 +1,13 @@
-# ask user for two numbers
+# Ask user for two numbers
 first_number = int(input("Enter your first number: "))
 second_number = int(input("Enter your second number: "))
 
-# check if the first number is greater than the second number, swap if first number is greater than second number
+# Check if the first number is greater than the second number, swap if first number is greater than second number
 if first_number > second_number:
     first_number, second_number = second_number, first_number
 
-# for loop all the numbers between the first and second number, excluding both
+# For loop all the numbers between the first and second number, excluding both
 for result in range(first_number +1, second_number):
-    # print the result
+    # Print the result
     print(result)
     
