8a2876c835d06485208305d0dd5a3a81bbf0a128
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:00:39 +0800

    program 05 final final

--- python_program_compilation_batch_1/05_quotient_of_two_numbers_with_decimal.py
+++ python_program_compilation_batch_1/05_quotient_of_two_numbers_with_decimal.py
@@ -1,9 +1,9 @@
-# ask user for two input
+# Ask user for two input
 first_number = float(input("Input the first number: "))
 second_number = float(input("Input the second number: "))
 
-# divide the two numbers
+# Divide the two numbers
 result = first_number / second_number
 
-# print the result or quotient
+# Print the result or quotient
 print(f"The quotient is: {result}")
