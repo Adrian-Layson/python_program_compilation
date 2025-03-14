aa9678acd66d9a5d7ee67af454e718917e7a5410
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:37:35 +0800

    program 01 final

--- python_program_compilation_batch_3/01_display_numbers_that_don't_have_duplicate.py
+++ python_program_compilation_batch_3/01_display_numbers_that_don't_have_duplicate.py
@@ -1,11 +1,14 @@
 #  make a list to store the numbers that will be inputted
 numbers = []
+
 # ask user for 10 inputs
 for i in range(10):
     num = int(input(f"Enter your {i + 1} number: "))
     numbers.append(num)
+
 # display the numbers that don't have duplicates
 print("Numbers that don't have duplicates: ")
+
 # check each number in the list
 for num in numbers:
     if numbers.count(num) == 1: # if the count of number is 1, it's unique
