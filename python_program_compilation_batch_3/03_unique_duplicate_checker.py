a6f9551cb3e06dd0908a2dc0d6fe37dbf5f3dd11
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:40:18 +0800

    program 03 final

--- python_program_compilation_batch_3/03_unique_duplicate_checker.py
+++ python_program_compilation_batch_3/03_unique_duplicate_checker.py
@@ -4,16 +4,16 @@ numbers = []
 while True:
     try:
         # ask user to input a number
-        num = float(input("Enter a number: "))
+        user_input = float(input("Enter a number: "))
 # check if the numbe has a duplicate
-        if numbers.count(num) == 0:
+        if numbers.count(user_input) == 0:
             print("Unique")
         else:
             print("duplicate")
 # add the number to the list for future comparison
-        numbers.append(num)
+        numbers.append(user_input)
 
     except ValueError:
 # if user enters a non-numerical input, exit the loop
-        print("Invalid input")
+        print("Invalid input. Exiting the program.")
         break
