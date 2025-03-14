de741724e8bd7a87f879230797f71481245abe2a
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:06:14 +0800

    program 01 final final

--- python_program_compilation_batch_2/01_print_the_smaller_number.py
+++ python_program_compilation_batch_2/01_print_the_smaller_number.py
@@ -1,11 +1,11 @@
-# ask user for two inputs
+# Ask user for two inputs
 first_number = int(input("Enter your first number: "))
 second_number = int(input("Enter your second number: "))
 
-# compare the two inputs
+# Compare the two inputs
 if first_number > second_number:
    
-   # print the smaller number
+   # Print the smaller number
     print(f"The smaller number is: {second_number}")
 elif first_number < second_number:
     print(f"The smaller number is: {first_number}")
