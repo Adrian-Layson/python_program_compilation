eb013bd2a24e39995ab8f6b79a55e48c95043876
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 14:23:42 +0800

    program 08 final

--- python_program_compilation_batch_1/08_odd_number_counter.py
+++ python_program_compilation_batch_1/08_odd_number_counter.py
@@ -1,5 +1,6 @@
 # set the odd counter to 0
 odd_count = 0
+
 # ask for 10 inputs
 for i in range(10):
     number = int(input(f"Enter number {i + 1}: "))
@@ -7,5 +8,6 @@ for i in range(10):
     if number % 2 != 0:
     # if odd, add one to odd counter
         odd_count += 1
+
 # print the total odd numbers
 print(f"The total count of odd numbers are : {odd_count}")
