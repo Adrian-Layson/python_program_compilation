4472633ea7969be48090b2fa9166f967369216a7
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:11:40 +0800

    program 09 final final

--- python_program_compilation_batch_2/09_print_all_numbers_from_0_to_100_except_10_or_5.py
+++ python_program_compilation_batch_2/09_print_all_numbers_from_0_to_100_except_10_or_5.py
@@ -1,8 +1,8 @@
-# make a variable with a value of 0 which where we will start
+# Make a variable with a value of 0 which where we will start
 current_number = 0
 
-# initialize a while loop that runs until the number is greater than 100
+# Initialize a while loop that runs until the number is greater than 100
 while current_number <= 100:
-    if current_number % 10 != 0 and current_number % 10 != 5: # check if the number is not divisible by 10 and 5
-        print(current_number) # print the current value of number if it doesn't meet the condition
+    if current_number % 10 != 0 and current_number % 10 != 5: # Check if the number is not divisible by 10 and 5
+        print(current_number) # Print the current value of number if it doesn't meet the condition
     current_number += 1 # Increment the number
