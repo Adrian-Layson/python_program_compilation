8e636ae72157704bc34620d9b3102d6a98629f65
Renser Ivahn <renserivahngutierrez@gmail.com>
2025-03-14 15:01:35 +0800

    program 06 final final

--- python_program_compilation_batch_1/06_first_number_raised_to_second_number.py
+++ python_program_compilation_batch_1/06_first_number_raised_to_second_number.py
@@ -1,9 +1,9 @@
-# input two numbers
+# Input two numbers
 first_number = int(input("Input the first number: "))
 second_number = int(input("Input the second number: "))
 
-# raise the first number by second number
+# Raise the first number by second number
 result = first_number ** second_number
 
-# print the result
+# Print the result
 print(f"The base {first_number} raised to {second_number} is: {result}")
